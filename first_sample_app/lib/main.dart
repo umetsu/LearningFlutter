778306946b5f9b752ddb63b0c6e122920c36daf4
umetsu <prunusmume0727@gmail.com>
2019-01-04 17:08:28 +0900

    [Write Your First Flutter App, part 1] 4. Use an external package

--- first_sample_app/lib/main.dart
+++ first_sample_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
@@ -5,14 +6,15 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: "Welcome to Flutter",
       home: new Scaffold(
         appBar: new AppBar(
           title: const Text("Welcome to Flutter"),
         ),
-        body: const Center(
-          child: const Text("Hello World"),
+        body: new Center(
+          child: new Text(wordPair.asPascalCase),
         ),
       ),
     );
