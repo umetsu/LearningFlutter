9bafc08867bae52d0a052e9163bfb34563bbe550
umetsu <prunusmume0727@gmail.com>
2019-02-01 11:55:36 +0900

    [Building Beautiful UIs with Flutter] 5. Add a UI for composing messages

--- codelab/friendlychat/lib/main.dart
+++ codelab/friendlychat/lib/main.dart
@@ -14,13 +14,52 @@ class FriendlychatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() {
+    return new ChatScreenState();
+  }
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Friendlychat"),
       ),
+      body: _buildTextComposer(),
     );
+  }
+
+  Widget _buildTextComposer() {
+    return new IconTheme(
+        data: new IconThemeData(color: Theme.of(context).accentColor),
+        child: new Container(
+            margin: const EdgeInsets.symmetric(horizontal: 8),
+            child: new Row(
+              children: <Widget>[
+                new Flexible(
+                  child: new TextField(
+                    controller: _textController,
+                    onSubmitted: _handleSubmitted,
+                    decoration: new InputDecoration.collapsed(
+                        hintText: "Send a message"),
+                  ),
+                ),
+                new Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 4),
+                  child: new IconButton(
+                      icon: new Icon(Icons.send),
+                      onPressed: () => _handleSubmitted(_textController.text)),
+                )
+              ],
+            )));
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
   }
 }
