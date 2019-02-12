1eb87fbf6b1dffbd92bad40b31eeb76e3a531e0a
umetsu <prunusmume0727@gmail.com>
2019-02-12 14:04:04 +0900

    [Building Beautiful UIs with Flutter] 6. Add a UI for displaying messages

--- codelab/friendlychat/lib/main.dart
+++ codelab/friendlychat/lib/main.dart
@@ -22,15 +22,30 @@ class ChatScreen extends StatefulWidget {
 }
 
 class ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
+
   final TextEditingController _textController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Friendlychat"),
+      appBar: new AppBar(title: new Text("Friendlychat")),
+      body: new Column(
+        children: <Widget>[
+          new Flexible(
+            child: new ListView.builder(
+                padding: new EdgeInsets.all(8),
+                reverse: true,
+                itemBuilder: (_, int index) => _messages[index],
+                itemCount: _messages.length),
+          ),
+          new Divider(height: 1),
+          new Container(
+            decoration: new BoxDecoration(color: Theme.of(context).cardColor),
+            child: _buildTextComposer(),
+          ),
+        ],
       ),
-      body: _buildTextComposer(),
     );
   }
 
@@ -61,5 +76,45 @@ class ChatScreenState extends State<ChatScreen> {
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    ChatMessage message = new ChatMessage(
+      text: text,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
   }
 }
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+            margin: const EdgeInsets.only(right: 16),
+            child: new CircleAvatar(child: new Text(_name[0])),
+          ),
+          new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Text(_name, style: Theme.of(context).textTheme.subhead),
+              new Container(
+                margin: const EdgeInsets.only(top: 5),
+                child: new Text(text),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+const String _name = "Umetsu";
